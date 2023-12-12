d270d6a0e2cde3a52cfb70cf6ef5520863ff4da4
KijitoraFInch <KijitoraFinch-Caffeine@proton.me>
2023-12-12 20:58:40 +0900

    おそすぎる最初のコミット

---
+++ dot_config/nvim/init.lua
@@ -0,0 +1,29 @@
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+-- [[ Install `lazy.nvim` plugin manager ]]
+require 'lazy-bootstrap'
+
+-- [[ Configure plugins ]]
+require 'lazy-plugins'
+
+-- [[ Setting options ]]
+require 'options'
+
+-- [[ Basic Keymaps ]]
+require 'keymaps'
+
+-- [[ Configure Telescope ]]
+-- (fuzzy finder)
+require 'telescope-setup'
+
+-- [[ Configure Treesitter ]]
+-- (syntax parser for highlighting)
+require 'treesitter-setup'
+
+-- [[ Configure coc.nvim  ]]
+require 'coc-setup'
+
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
